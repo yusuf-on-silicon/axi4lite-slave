17cda82499d3e0c3d21886795bc6efcf1b9f9c56
Yusuf Ahmad <yusufahmad.mail@gmail.com>
2025-08-19 23:48:28 +0530

    changes made to memory - can read and write, but strobe not implemented yet, fixed formating on main and memory submodule

--- src/main.v
+++ src/main.v
@@ -1,32 +1,34 @@
 module moduleName #(
-parameter dataWidth = 32;
-parameter addrWidth = 6 ;
+parameter dataWidth = 32 ,
+parameter addrWidth = 5  ,
+parameter strbWidth = (dataWidth/8) - 1 
+
 )(
-input  wire                 clk     ;
-input  wire                 reset   ;
+input  wir                clk     ,
+input  wir                reset   ,
 
-input  wire                 AWVALID ;
-input  wire [addrWidth:0]   AWADDR  ;
-output wire                 AWREADY ;
+input  wir                AWVALID ,
+input  wire[addrWidth:0]  AWADDR  ,
+output wire               AWREADY ,
 
-input  wire                 WVALID  ;
-input  wire [dataWidth:0]   WDATA   ;
-input  wire [3:0]           WSTRB   ;
-output wire                 WREADY  ;
+input  wire               WVALID  ,
+input  wire[dataWidth:0]  WDATA   ,
+input  wire[3:0]          WSTRB   ,
+output wire               WREADY  ,
 
-input  wire                 BREADY  ;
-output wire [1:0]           BRESP   ;
-output wire                 BVALID  ;
+input  wire               BREADY  ,
+output wire[1:0]          BRESP   ,
+output wire               BVALID  ,
 
-input  wire                 ARVALID ;
-input  wire [addrWidth:0]   ARADDR  ;
-output wire                 ARREADY ;
+input  wire               ARVALID ,
+input  wire[addrWidth:0]  ARADDR  ,
+output wire               ARREADY ,
 
-input  wire                 RREADY  ;
-output wire [dataWidth:0]   RDATA   ;
-output wire [1:0]           RRESP   ;
-output wire                 RVALID  ;
+input  wire               RREADY  ,
+output wire[dataWidth:0]  RDATA   ,
+output wire[1:0]          RRESP   ,
+output wire               RVALID  
 );
 
-
+wire WEN,REN = 0;
 endmodule
--- src/memory.v
+++ src/memory.v
@@ -1,23 +1,38 @@
 module moduleName #(
-    parameter addrWidth = 6 , 
-    parameter dataWidth = 32, 
-    parameter dataDepth = 64 
+    parameter addrWidth = 5  ,
+    parameter dataWidth = 32 ,
+    parameter dataDepth = 64 ,
+    parameter strbWidth = (dataWidth/8) - 1,
 ) (
-    input  wire  clk    ,
-    input  wire  reset  ,
+    input  wire               clk    ,
+    input  wire               reset  ,
 
-    input  wire  AWADDR ,
-    input  wire  WDATA  ,
+    input  wire               WEN    ,
+    input  wire[addrWidth:0]  AWADDR ,
+    input  wire[strbWidth:0]  WSTRB  ,
+    input  wire[dataDepth:0]  WDATA  ,
 
-    input  wire  ARADDR ,
-    output wire  ARADDR 
+    input  wire               REN    ,
+    input  wire[addrWidth:0]  ARADDR ,
+    output wire[dataDepth:0]  RDATA 
 );
     
 reg [dataDepth:0] memory[dataWidth:0];
+reg []
+
 always @(posedge clk or negedge reset) begin
     if (!reset) begin
-
-    end else if ()
+        RDATA <= 32'b0;
+    end else if (WEN) begin
+        RDATA <= memory[RADDR];
+    end
+end
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
+        RDATA <= 32'b0;
+    end else if (WEN) begin
+        RDATA <= memory[RADDR];
+    end
 end
 
 endmodule
